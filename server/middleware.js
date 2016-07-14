ebc3f9e715a679b9fb1af4175849103f699fecdd
alicekao <alicekao.ak@gmail.com>
2016-07-14 19:10:48 -0400

    Added conditional for mogan in dev mode

--- server/middleware.js
+++ server/middleware.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const path = require('path');
-const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 module.exports = function (app) {
-  app.use(morgan('dev'));
+  if (process.env.NODE_ENV !== 'production') {
+    const morgan = require('morgan');
+    app.use(morgan('dev'));
+  }
+  
   app.use(bodyParser.json({ type: '*/*' }));
 
   app.use(express.static(path.join(__dirname, '../dist')));
 
-  app.get('*', function(req, res) {
+  app.get('*', function (req, res) {
     res.sendFile(path.resolve(__dirname, '../dist', 'index.html'));
   });
 };
