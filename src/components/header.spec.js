ffd79fe20d091c1f7214ea69b2c40bf06c09f5ca
alicekao <alicekao.ak@gmail.com>
2016-08-10 23:20:55 -0400

    Completed shallow rendering header component

--- src/components/header.spec.js
+++ src/components/header.spec.js
@@ -6,7 +6,27 @@ import {Header} from './header';
 describe('Header', () => {
   it('Should have nav items', () => {
     const wrapper = shallow(<Header />);
+    expect(wrapper.find('Link').first().html().includes('PinPointMe')).toBe(true);
     expect(wrapper.find('nav').length).toEqual(1);
+  });
+  it('Should show sign in if not signed in', () => {
+    const spy = createSpy();
+    const wrapper = shallow(<Header isAuth={false} logoutUser={spy}/>);
+    expect(wrapper.find('.nav-link').html().includes('Sign in')).toBe(true);
+    expect(wrapper.find('.nav-link').html().includes('Sign out')).toBe(false);
+    wrapper.find('Link').at(1).simulate('click');
+    expect(spy).toNotHaveBeenCalled();
+  });
+  it('Should show sign out if signed in', () => {
+    const wrapper = shallow(<Header isAuth={true} />);
+    expect(wrapper.find('.nav-link').html().includes('Sign out')).toBe(true);
+    expect(wrapper.find('.nav-link').html().includes('Sign in')).toBe(false);
+  });
+  it('Should handle logout', () => {
+    const spy = createSpy();
+    const wrapper = shallow(<Header isAuth={true} logoutUser={spy}/>);
+    wrapper.find('a').simulate('click');
+    expect(spy).toHaveBeenCalled();
   });
 });
 
